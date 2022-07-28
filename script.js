115229257ae90b3ec7c4d81daddc9fdcbb2caeaa
Davi Moisés <moisesdavi005@gmail.com>
2022-07-28 18:20:57 -0300

    entreag_extra_metodos

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-//Método forEach --------
-const arrayForEach = [10, 20, 30, 44, 55]; // array para iteração do método
+//Método forEach
+const arrayForEachUm = [10, 20, 30, 44, 55]; // array para iteração do método
 
 //array em que nossa callback vai colocar os números multiplicados
-const arrayVazio = [];
+const arrayVazioUM = [];
 //nossa callback
-function callbackForEach(element) {
-  return arrayVazio.push(element * 2);
+function callbackForEachUm(element) {
+  return arrayVazioUM.push(element * 2)
 }
 
-function forEach(array, callback) {
-  //sua lógica aqui
+function forEachUm(array, callback) {
+
+  /*  let oisis = []  */
+
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i])
+
+  } //sua lógica
+
 }
 
-// console.log(forEach(arrayForEach, callbackForEach));
-// console.log(arrayVazio);
+console.log(forEachUm(arrayForEachUm, callbackForEachUm)); //undefined
+console.log(arrayVazioUM); // (5) [20, 40, 60, 88, 110]
 
 //Método Find --------
 
@@ -34,8 +41,32 @@ function callbackFindTwo(element) {
 }
 
 function find(array, callback) {
+
+  let arrayFind = []
+
+  for (let i = 0; i < array.length; i++) {
+    let numeros = array[i]
+    arrayFind.push(numeros)
+  }
+
+  return callback(arrayFind)
   //sua lógica aqui
 }
+console.log(find(arrayFind, function (element) {
+  for (let i = 0; i < element.length; i++) {
+    if (element[i] > 2 && element[i] < 12) {
+      return true;
+    }
+  }
+
+}))
+console.log(find(arrayFind, function (element) {
+  for (let i = 0; i < element.length; i++) {
+    if (element[i] > 15) {
+      return false;
+    }
+  }
+}))
 // console.log(find(arrayFind, callbackFind));
 // console.log(find(arrayFind, callbackFindTwo));
 
@@ -54,31 +85,37 @@ function callbackIndexOfTwo(element) {
 }
 
 function indexOf(array, callback) {
+
+  let arrayIndexOf = []
+
+  for (let i = 0; i < array.length; i++) {
+    arrayIndexOf.push(array[i])
+  }
+
+  return callback(arrayIndexOf)
   //sua lógica aqui
 }
-// console.log(indexOf(arrayIndexOf, callbackIndexOf));
-// console.log(indexOf(arrayIndexOf, callbackIndexOfTwo));
+
+console.log(indexOf(arrayIndexOf, callbackIndexOf));
+console.log(indexOf(arrayIndexOf, callbackIndexOfTwo));
 
 //Método Includes --------
 
 const arrayIncludes = [53, 2, "duck", NaN, 456, 7]; // array para iteração do método
 
-//callback que procura algum elemento no array maior que 500
-function callbackIncludesArray(element) {
-  return element > 500 && true;
+function includes(array, searchElement) {
+
+  for (let i = 0; i < array.length; i++) {
+    if (searchElement == array[i]) {
+      return true
+    }
+  }
+  return false
+  //sua lógica
 }
 
-//callback que procura no elemento string se o conjunto de caracteres é encontrado
-function callbackIncludesString(element) {
-  return element === "duck" && true;
-}
-
-function includes(array, callback) {
-  //sua lógica aqui
-}
-
-// console.log(includes(arrayIncludes, callbackIncludesArray));
-// console.log(includes(arrayIncludes, callbackIncludesString));
+console.log(includes(arrayIncludes, "duck")); //true
+console.log(includes(arrayIncludes, 500)); //false
 
 //Método some --------
 const arrySome = [12, 82, 563, 414, null]; // array para iteração do método
@@ -93,20 +130,65 @@ const callbackSome2 = (element) => {
 };
 
 function some(array, callback) {
+
+  let arraySoma = []
+
+  for (let i = 0; i < array.length; i++) {
+    arraySoma.push(array[i])
+  }
+  return callback(arraySoma)
   //sua lógica aqui
 }
 
-// console.log(some(arrySome, callbackSome));
-// console.log(some(arrySome, callbackSome2));
+console.log(some(arrySome, callbackSome));
+console.log(some(arrySome, callbackSome2));
 
 //Método Join --------
 
 const arrayJoin = ["Fogo", "no", "Parquinho"]; // array para iteração do método
 
 function join(array, value) {
+
+  let strDeJoin = ""
+
+  for (let i = 0; i < array.length; i++) {
+    strDeJoin += `${array[i]}${value}`
+  }
+  return strDeJoin
   //sua lógica aqui
 }
+console.log(arrayJoin, join(arrayJoin, " "));
+console.log(arrayJoin, join(arrayJoin, ""));
+console.log(arrayJoin, join(arrayJoin, "-"));
 
-// console.log(join(arrayJoin, " "));
-// console.log(join(arrayJoin, ""));
-// console.log(join(arrayJoin, "-"));
+//Método forEach --------
+//const arrayForEach = [10, 20, 30, 44, 55]; // array para iteração do método
+
+//array em que nossa callback vai colocar os números multiplicados
+// const arrayVazio = [];
+//nossa callback
+//function callbackForEach(element) {
+//  return arrayVazio.push(element * 2);
+//}
+
+//function forEach(array, callback) {
+
+  //let arrayUm = []
+
+//  for (let i = 0; i < array.length; i++) {
+//    let numero = array[i]
+ //   arrayUm.push(numero)
+ /// }
+  //sua lógica aqui
+ // return callback(arrayUm)
+//}
+//console.log(forEach(arrayForEach, function (element) {
+//  let arrayDois = []
+ // for (let i = 0; i < element.length; i++) {
+//    arrayDois.push(element[i] * 2);
+ // }
+ // return arrayDois
+//}))
+/* console.log(forEach(arrayForEach, callbackForEach)); //undefined
+console.log(arrayVazio); 
+ */
